01a234f70314dd58d410113b637c81e167a63c11
Qurroayun <kuriayun@gmail.com>
2025-08-09 00:56:06 +0700

    layout update

--- src/app/landing/layout.tsx
+++ src/app/landing/layout.tsx
@@ -1,6 +1,6 @@
 // src/app/(landing)/layout.tsx
 import { ReactNode } from "react";
-import Navbar from "@/components/landing/navbar/Navbar";
+import Navbar from "@/components/landing/navbar/navbar";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export default function LandingLayout({ children }: { children: ReactNode }) {
